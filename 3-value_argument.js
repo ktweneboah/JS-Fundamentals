16c73e482778d61d69c8477d2425e866b2433678
ktweneboah <117413003+ktweneboah@users.noreply.github.com>
2025-11-09 23:38:34 +0000

    Update console messages for argument checks

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -3,7 +3,7 @@ const args = process.argv.slice(2);
 if (args[0] === undefined) {
   console.log("No argument");
 } else if (args[1] === undefined) {
-  console.log("Argument found");
+  console.log("HBTN");
 } else {
-  console.log("Arguments found");
+  console.log("HBTN cool");
 }
