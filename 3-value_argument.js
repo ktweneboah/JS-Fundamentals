63f55d4c20fc7a6dacdf847d776cabf9b4268951
ktweneboah <117413003+ktweneboah@users.noreply.github.com>
2025-11-09 23:37:24 +0000

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,8 +1,8 @@
 const args = process.argv.slice(2);
 
-if (args.length === 0) {
+if (args[0] === undefined) {
   console.log("No argument");
-} else if (args.length === 1) {
+} else if (args[1] === undefined) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
