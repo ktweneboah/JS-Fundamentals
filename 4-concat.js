953a7ed649060399272b52ade340a8eedc6abbeb
ktweneboah <117413003+ktweneboah@users.noreply.github.com>
2025-11-10 00:17:02 +0000

    Correct console output and syntax in 4-concat.js
    Fix syntax error and update console log messages.

--- 4-concat.js
+++ 4-concat.js
@@ -1,7 +1,7 @@
 const args = process.argv.slice(2);
 
 if (args[0] === undefined) {
-  console.log("P");
+  console.log("Python is fun");
 } else if (args[1] === undefined{
   console.log("HBTN");
 } else {
